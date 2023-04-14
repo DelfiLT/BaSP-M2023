8500416af13dec0081423b70942ca5356ef62e28
Delfina Tomasino <tomasinodelfina@gmail.com>
2023-04-14 16:35:55 -0300

    BaSP-W05-2023: Exercise 1

---
+++ Week-05/exercises/01-variables-and-operators.js
@@ -0,0 +1,24 @@
+console.log('-------------------------------------');
+console.log('EXERCISE 1 : VARIABLES AND OPERATORS');
+console.log('-------------------------------------');
+
+console.log('EXERCISE 1A');
+
+var firstNum = 3;
+var secondNum = 5;
+
+var add = firstNum + secondNum;
+console.log('Answer: ' + add);
+
+console.log('EXERCISE 1B')
+
+var firstString = 'Radium';
+var secondString = 'Rocket';
+
+var stringAdd = firstString + secondString;
+console.log('Answer: ' + stringAdd);
+
+console.log('EXERCISE 1C')
+
+var lenghtAdd = firstString.length + secondString.length;
+console.log('Answer: ' + lenghtAdd);
