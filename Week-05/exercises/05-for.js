f18ebced3f20c3dba39f2e059903f56814102fb7
Delfina Tomasino <tomasinodelfina@gmail.com>
2023-04-15 17:30:17 -0300

    BaSP-W05-2023: Exercise 5 For

---
+++ Week-05/exercises/05-for.js
@@ -0,0 +1,37 @@
+console.log('-------------------------------------');
+console.log('        EXERCISE 5 : FOR             ');
+console.log('-------------------------------------');
+
+console.log('EXERCISE 5A');
+
+var words = ['Hello', 'everyone', 'this', 'is', 'RadiumRocket'];
+
+for (i = 0 ; i < words.length; i++) {
+    console.log(words[i]);
+}
+
+console.log('EXERCISE 5B');
+
+for (i = 0 ; i < words.length; i++) {
+    console.log(words[i][0].toUpperCase() + words[i].substring(1));
+}
+
+console.log('EXERCISE 5C');
+
+var sentence = '';
+
+for (i = 0 ; i < words.length; i++) {
+    sentence = sentence + ' ' +  words[i];
+}
+
+console.log('Answer: ' + sentence);
+
+console.log('EXERCISE 5D');
+
+var emptyArray = [];
+
+for (i = 0 ; i < 10; i++) {
+    emptyArray.push(i);
+}
+
+console.log('Answer: ', emptyArray);
